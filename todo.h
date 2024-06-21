f4cb56009cb75bd996251108bb52b308ab121426
chashkakefira <prokofievdaniil@mail.klaaan.ru>
2024-06-21 19:47:32 +0300

    Changed todo.h to use unsigned ints (use less memory)

--- todo.h
+++ todo.h
@@ -1,10 +1,16 @@
 typedef struct{
+	unsigned int day:4;
+	unsigned int month:3;
+	unsigned short year;
+} date;
+
+typedef struct{
 	const char *name;
 	const char *desc;
-	short prior;
-	char deadline[11];
+	unsigned int prior:3;
+	date deadline;
 } task;
 
-void newtask(char name[80], char desc[200], char deadline[11], short prior, FILE *wr){
-	fprintf(wr, "%s\n%s\n%s\n%i\n", name, desc, deadline, prior);
+void newtask(task task, FILE *wr){
+	fprintf(wr, "%s\n%s\n%s\n%i\n", task.name, task.desc, task.deadline, task.prior);
 }
